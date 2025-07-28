b05599dcc50be19598653ff3c0801babbab1b575
Utshah Neupane <utshah777@gmail.com>
2025-07-28 18:11:30 -0500

    databse initialized

--- container/__init__.py
+++ container/__init__.py
@@ -1,8 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-from container import routes
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///container.db'
+
+#So that users can't tamper session data stored on their browser
+#For flask-forms, protection to generate secure tokens
+app.config['SECRET_KEY'] = 'ca6db900e69ab97455788434'   
+
+db = SQLAlchemy(app)
+
+
+from container import routes, models
 
 
 
